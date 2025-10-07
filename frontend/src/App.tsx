43c4cc899a31bfcacc9812e4f5aaa4e228c35301
jangomax <jangomaxstudio@gmail.com>
2025-10-07 16:44:03 -0700

    Frontend config setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Container, Title, Stack } from '@mantine/core'
+
+function App() {
+  return (
+    <Container size="md" py="xl">
+      <Stack>
+        <Title order={1}>Kipu</Title>
+      </Stack>
+    </Container>
+  )
+}
+
+export default App
