16b899977ab3898ea460dc07c10c07b5e353d8b5
jangomax <jangomaxstudio@gmail.com>
2025-10-07 17:40:29 -0700

    Switch to AppShell

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import { RouterProvider } from 'react-router';
-import { Masthead } from '@/components/shared';
 import router from '@/routes';
+import { AppShell, Title } from '@mantine/core';
 
 function App() {
   return (
     <>
-      <Masthead />
-      <RouterProvider router={router} />
+      <AppShell padding="md" header={{ height: 50 }}>
+        <AppShell.Header style={{ display: 'flex', alignItems: 'center', paddingLeft: '1rem' }}>
+          <Title order={2}>Kipu</Title>
+        </AppShell.Header>
+        <AppShell.Main>
+          <RouterProvider router={router} />
+        </AppShell.Main>
+      </AppShell>
     </>
   );
 }
