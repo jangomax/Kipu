1ed84fe2b52c1e37a88986fb1f9c618aa1bb1206
jangomax <jangomaxstudio@gmail.com>
2025-10-15 01:12:16 -0700

    Remove unused imports

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
-import { RouterProvider } from 'react-router';
-import '@mantine/core/styles.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
